56c5129bc4585b1803488538d12697cab8052833
Ian Link <ian@linkportfolio.net>
2012-03-25 17:27:25 -0700

    timeline can now pause, resume safely. Also removed some old date stuff. And added some more math test functions.

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
-var timeline = new Timeline({
-    tickrate: "10",
-    callback: function(dt){ // render loop goes here
-        var pi = 0;
-        var n = 1;
-        for(var i = 0; i < 100000; i++){
-            pi = pi + (4 / n) - (4 / (n + 2));
-            n = n + 4;
-        }
-    }
-});
+var Animation = function(args){
+    var x = 0;
+    var mover = $(args.mover);
 
+    var draw_at = function(element){
+        $(element).clone().appendTo('#display').text("*");
+    }
+
+    this.sin = function(dt){
+        x += dt / 100;
+
+        var sin_x = Math.sin(x);
+        sin_x *= 50;
+        mover.css({
+            "top": sin_x,
+            "left": x * 10
+        });
+        draw_at(mover);
+    }
+
+    this.bounce = function(dt){
+        x += dt / 100;
+        var y = Math.abs(Math.sin(x) / x) * 100;
+        mover.css({
+            "bottom": y * 10,
+            "left": x * 10
+        });
+        draw_at(mover);
+    }
+
+}
+
+var pi = function(){
+    var pi = 0;
+    var n = 1;
+    for(var i = 0; i < 1000000; i++){
+        pi = pi + (4 / n) - (4 / (n + 2));
+        n = n + 4;
+    }
+}
 
 $(document).ready(function(){
-    timeline.start();
-    timeline.stop();
+
+    var animation = new Animation({
+        mover: "#mover"
+    });
+
+    var timeline = new Timeline({
+        tickrate: 10,
+        callback: function(dt){ // render loop goes here
+            animation.sin(dt);
+
+            pi();
+        }
+    });
 
     $("#start").click(function(){
         timeline.start();
--- time.js
+++ time.js
@@ -1,36 +1,42 @@
 var Timeline = function(args){
-    var dt, dt_floor, dt_ceil, time_start, time_end, interval, callback, tickrate;
-    dt_floor = args.dt_floor || 0.066; // this is the lowest time delta that will ever get passed
-    dt_ceil = args.dt_ceil || 2; // this is the ceiling on dt. Not used yet, since there is no proper pausing functionality
-    tickrate = args.tickrate;
+    var dt, dt_floor, dt_ceil, time_start, time_end, interval, callback, tickrate, paused, running;
+    // all time delta values are in mS
+    // javascript does not seem to get more granular than mS
+    dt_floor = args.dt_floor || 30; // this is the lowest time delta that will ever get passed
+    dt_ceil = args.dt_ceil || 40; // this is the ceiling on dt. Not used yet, since there is no proper pausing functionality
+    tickrate = args.tickrate; // how often the loop should run
     callback = args.callback;
 
     var loop = function(dt){
         interval = setTimeout(function(){
             time_start = new Date();
             callback(dt);
-            console.log(dt);
+//            console.log(dt);
             time_end = new Date();
             dt = time_end - time_start;
-            if(dt < 1) dt = 0.066;
+            if(dt < 1) dt = dt_floor;
+            else console.log(dt);
             loop(dt);
         }, tickrate);
     }
 
     this.start = function(){
-        time_start = (new Date()).getTime();
-        if(typeof dt === 'undefined') dt = dt_floor;
+        if(!running){
+            running = true;
+            paused = false;
+            if(typeof dt === 'undefined') dt = dt_floor;
 
-        if (typeof(callback !== 'undefined') && callback instanceof Function){
-            callback(dt);
-            loop(dt);
+            if (typeof callback !== 'undefined' && callback instanceof Function){
+                callback(dt);
+                loop(dt);
+            }
+            else return "error, no function provided";
         }
-        else return "error, no function provided";
-
-        time_end = (new Date()).getTime();
     }
 
     this.stop = function(){
+        running = false;
+        paused = true;
         if (interval !== null) {
             clearInterval(interval);
             interval = null;
