8e894e9d8b18acc0d0d1697ec6887e46dc0286ae
Ian Link <ian@linkportfolio.net>
2012-04-09 07:45:53 -0700

    density diffuse progress

--- density.js
+++ density.js
@@ -6,6 +6,7 @@ var Density = function(args){
         for ( var k = 0; k < 20; k++ ) {
             for ( var i = 1; i<= size; i++ ) {
                 for ( var j = 1 ; j <= size; j++ ) {
+                    // i think it is using the previous density (d0) because this process is iterative (see outermost loop
                     dens[i][j] = (dens0[i][j] + a * (
                                                     dens[i-1][j] + dens[i+1][j] + dens[i][j-1] + dens[i][j+1])
                                                     ) / (1 + 4 * a);
