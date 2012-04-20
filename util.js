c5ccc5e8fe1871a091b3ad015d6a808054bb07db
Ian Link <ian@linkportfolio.net>
2012-04-20 13:14:16 -0700

    improved swap

--- util.js
+++ util.js
@@ -2,35 +2,35 @@ var Util = function(){};
 
 /*
     @swap
-    This is a lazy way of writing this function. It could be done (for objects) in-place, since objects and arrays are passed by reference.
-    However, I think that is as good as it gets for primitive types
+    objects and arrays muat have the same number of elements / properties
+    objects must have the same property names
 
     Usage:
 
     a = 1; b = 2;
     result = swap(a,b);
     a = result[0]; b = result[1];
- */
-Util.prototype.swap = function(a, b){
+*/
+Util.prototype.swap = function(a, b) {
     var temp;
-    if(typeof a !== typeof b) throw 'type mismatch: both arguments must be the same type';
-    if( typeof a !== 'object' || typeof a !== 'array'){
-            temp = a;
-            a = b;
-            b = temp;
-        return [a, b]
-    } else if(typeof a === 'array') {
-        temp = $.extend(true, [], a);
+    if (typeof a !== typeof b) throw 'type mismatch: both arguments must be the same type';
+    if (typeof a !== 'object' && typeof a !== 'array') {
+        temp = a;
         a = b;
         b = temp;
         return [a, b]
+    } else if (typeof a.length !== 'undefined') {
+        if (a.length !== b.length) throw 'Error: arrays must have the same length';
+        temp = $.extend(true, [], a);
     }
     else {
-        temp = $.extend(true, [], a);
-        a = b;
-        b = temp;
-        return [a, b]
+        temp = $.extend(true, {}, a);
     }
+    $.each(a, function(i, v){
+        a[i] = b[i];
+        b[i] = temp[i];
+    });
+    return true
 }
 
 Util.prototype.init_2d_arr = function(size, arr){
