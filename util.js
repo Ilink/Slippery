914d67330de3072be419d8051bba9a5e9ec272d1
Ian Link <ian@linkportfolio.net>
2012-04-20 11:52:19 -0700

    swap function

--- util.js
+++ util.js
@@ -1,6 +1,10 @@
 var Util = function(){};
 
 /*
+    @swap
+    This is a lazy way of writing this function. It could be done (for objects) in-place, since objects and arrays are passed by reference.
+    However, I think that is as good as it gets for primitive types
+
     Usage:
 
     a = 1; b = 2;
@@ -8,8 +12,30 @@ var Util = function(){};
     a = result[0]; b = result[1];
  */
 Util.prototype.swap = function(a, b){
-    var temp = a;
-    a = b;
-    b = temp;
-    return [a, b]
+    var temp;
+    if(typeof a !== typeof b) throw 'type mismatch: both arguments must be the same type';
+    if( typeof a !== 'object' || typeof a !== 'array'){
+            temp = a;
+            a = b;
+            b = temp;
+        return [a, b]
+    } else if(typeof a === 'array') {
+        temp = $.extend(true, [], a);
+        a = b;
+        b = temp;
+        return [a, b]
+    }
+    else {
+        temp = $.extend(true, [], a);
+        a = b;
+        b = temp;
+        return [a, b]
+    }
+}
+
+Util.prototype.init_2d_arr = function(size, arr){
+    for(var i = 0; i < size; i++){
+        arr[i] = [];
+    }
+    return arr;
 }
