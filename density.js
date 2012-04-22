91f825cad9b2facbbf9c81d1b0e9e774ad50f974
Ian Link <ian@linkportfolio.net>
2012-04-21 18:55:39 -0700

    added density step moved diffuse out of density

--- density.js
+++ density.js
@@ -1,29 +1,20 @@
 var Density = function(args){
     var self = this;
+    var size = args.size;
 
-    var diffuse = function(size, bound, dens, dens0, rate, dt){
-        var a = dt * rate * size * size;
-        for (var k = 0; k < 20; k++ ) {
-            for (var i = 1; i<= size; i++ ) {
-                for (var j = 1 ; j <= size; j++ ) {
-                    // i think it is using the previous density (d0) because this process is iterative (see outermost loop)
-                    // could dens0 be part of the function instead of an argument?
-                    dens[i][j] = (dens0[i][j] + a * (
-                                                    dens[i-1][j] + dens[i+1][j] + dens[i][j-1] + dens[i][j+1])
-                                                    ) /
-                                                    (1 + 4 * a);
-                }
-            }
-            self.set_boundary (size, bound, dens);
-        }
-    }
-
-    this.step = function(dens, u, v){
-        diffuse();
+    // x is density
+    // u, v are velocity vectors
+    this.step = function(x, x0, u, v, diff, dt){
+        self.increase(size, x, x0, dt);
+        self.swap(x, x0);
+        self.diffuse();
+        self.swap(x, x0);
         self.advect();
     }
 }
 
 Density.prototype.set_boundary = Fluid_core.prototype.set_boundary;
+Density.prototype.diffuse = Fluid_core.prototype.diffuse;
 Density.prototype.advect = Fluid_core.prototype.advect;
+Density.prototype.increase = Fluid_core.prototype.increase;
 Density.prototype.swap = Util.prototype.swap;
